7cbaba9c5d9ee6d2bbc13d9f680551b9b741d547
“Foxshane997” <Foxshane997@gmail.com>
2024-09-16 18:30:51 -0700

    pushing to repo

--- index.js
+++ index.js
@@ -1,117 +1,98 @@
-// The array for the questionsAndAnswers, Answers & the Correct Answers.
-const questionsAndAnswers = [
-    {
-        question: "Who cut off Anakin's hand?",
-        answers: ["Asaj Ventress", "Count Doku", "Darth Vader"],
-        correctAnswer: "Count Doku"
-    },
-    {
-        question: "What is Count Doku's Sith name?",
-        answers: ["Darth Doku", "Count Doku", "Darth Tyranus"],
-        correctAnswer: "Darth Tyranus"
-    },
-    {
-        question: "What planet is Chewbacca from?",
-        answers: ["Kashyyk", "Hoth", "Corellia"],
-        correctAnswer: "Kashyyk"
-    },
-    {
-        question: "What is Rey's current last name?",
-        answers: ["Skywalker", "Palpatine", "Just Rey"],
-        correctAnswer: "Skywalker"
-    },
-    {
-        question: "Who created C3PO?",
-        answers: ["Luke Skywalker", "Anakin Skywalker", "Mace Window"],
-        correctAnswer: "Anakin Skywalker"
-    },
-    {
-        question: "What was Luke Skywalkers original name going to be?",
-        answers: ["Master Luke", "Luke Skywalker", "Luke Starkiller"],
-        correctAnswer: "Luke Starkiller"
-    }
-];
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Star Wars Quiz Game With Sass Styling</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="./CSS/style.css" />
+  </head>
+  <body>
+    <img
+      class="hide"
+      src="./assets/StarWars Galaxys Edge Pexels.jpg"
+      alt="Disney Galaxy's Edge Storm Trooper by Craig Adderley from Pexels"
+    />
 
-// Declaring these globally so they can be update when needed.
-let currentQuestionIndex = 0;
-let correctAnswers = 0;
+    <div class="container">
+  
+      <div class="start-screen">
+        <h1 class="heading">Star Wars Quiz With Sass</h1>
+        <div class="settings">
+ 
+          <button class="btn start">Start Quiz!</button>
 
-// Function to display the current question and answers.
-function displayQuestion() {
-    const question = questionsAndAnswers[currentQuestionIndex];
-    document.querySelector('.question').textContent = question.question;
-    const answerElements = document.querySelectorAll('.answer');
-    answerElements.forEach((answerElement, index) => {
-        answerElement.querySelector('.text').textContent = question.answers[index];
-        // Remove the 'selected' class from all answer elements.
-        answerElement.classList.remove('selected');
-        // Add event listener for when you choose an answer or when you pick a different answer.
-        answerElement.addEventListener('click', () => {
-            // Remove the coloring for when you choose a different answer.
-            answerElements.forEach((element) => {
-                element.classList.remove('selected');
-            });
-            // Add the coloring for when you choose your answer.
-            answerElement.classList.add('selected');
-        });
-    });
-    document.querySelector('.current').textContent = currentQuestionIndex + 1; // Update current question number after clicking next.
-    document.querySelector('.total').textContent = questionsAndAnswers.length; // Update total number of questions that are displayed above and at the end.
-}
 
-// Function to handle when the "Next" button is clicked.
-function onNextButtonClick() {
-    // This is pulling the questions & answers.
-    const question = questionsAndAnswers[currentQuestionIndex];
-    const selectedAnswer = document.querySelector('.answer.selected .text').textContent;
-    if (selectedAnswer === question.correctAnswer) {
-        correctAnswers++;
-    }
+          <div class="instruct-cont">
+            <ul>
+              <li class="instruct-list">
+                Click on the answer you think is correct.
+              </li>
+              <li class="instruct-list">
+                Once an answer is selected, click "Next" to move on.
+              </li>
+              <li class="instruct-list">
+                * You cannot continue if you haven't chosen an answer.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
-    // iterating through the question & answer array length
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questionsAndAnswers.length) {
-        displayQuestion();
-    } else {
-        // Quiz is over, show end screen after clicking the final next button.
-        document.querySelector('.end-screen').classList.remove('hide');
-        document.querySelector('.question-wrapper').classList.add('hide');
-        document.querySelector('.final-score').textContent = correctAnswers;
-        document.querySelector('.total-score').textContent = '/ ' + questionsAndAnswers.length; // Add slash here
-        document.querySelector(".restart").classList.remove('hide')
-    }
-}
 
-// Event listener for the "Next" button.
-document.querySelector('.next').addEventListener('click', onNextButtonClick);
+      <div class="question-wrapper hide">
+        <div class="number">
+          Question <span class="current">1</span> of <span class="total">6</span>
+        </div>
+        <div class="question">This is a question</div>
+      </div>
 
-// Function to start the quiz.
-function startQuiz() {
-    currentQuestionIndex = 0;
-    correctAnswers = 0; // Resets correct answers count.
-    displayQuestion();
 
-    //  Adding the correct answers in the console when you start the game.
-    questionsAndAnswers.forEach((item, index) => {
-        console.log(`Question ${index + 1}: ${item.question}`);
-        console.log(`Correct Answer: ${item.correctAnswer}`);
-        console.log(); // Empty line for separation in the order they are written.
-    });
+      <div class="answer-wrapper hide">
+        <div class="answer">
+          <span class="text">Answer 1</span>
+          <span class="checkbox">
+            <span class="icon">✓</span>
+          </span>
+        </div>
+        <div class="answer">
+          <span class="text">Answer 2</span>
+          <span class="checkbox">
+            <span class="icon">✓</span>
+          </span>
+        </div>
+        <div class="answer">
+          <span class="text">Answer 3</span>
+          <span class="checkbox">
+            <span class="icon">✓</span>
+          </span>
+        </div>
+      </div>
 
-    // Show the question wrapper and hide start screen.
-    document.querySelector('.question-wrapper').classList.remove('hide');
-    document.querySelector('.start-screen').classList.add('hide');
-}
 
-// Event listener for the start button to start the game.
-document.querySelector('.start').addEventListener('click', () => {
-    startQuiz();
-});
+      <button class="btn next hide">Next</button>
 
-// The restart button functionality.
-document.querySelector('.restart').addEventListener('click', () => {
-    // When you click Restart it hides the "end screen".
-    document.querySelector('.end-screen').classList.add('hide');
-    // When you click Restart it removes the Class hide from the "Start Screen".
-    document.querySelector('.start-screen').classList.remove('hide');
-});
+
+      <div class="end-screen hide">
+        <h1 id="heading-2" class="heading">The End!</h1>
+        <div class="score-cont">
+          <div class="score">
+            <span class="score-text">Your Score:</span>
+            <div>
+              <span class="final-score">0</span>
+              <span class="total-score">/10</span>
+            </div>
+          </div>
+        </div>
+        <button class="btn restart hide">Restart Quiz</button>
+      </div>
+    </div>
+
+    <script src="./index.js"></script>
+  </body>
+</html>
